4dd6c9c707e9720de96762cd7a91ce5ff650cace
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2023-09-28 23:24:41 +0300

    auto_daily_debit prepare

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import multiprocessing as mp
+import time
+
+def auto_daily_debit():
+    while True:
+        print('debit')
+        time.sleep(1)
 
 def main():
-    print('hello world')
+    time.sleep(5)
+    raise ValueError()
 
 
 if __name__ == '__main__':
+    p = mp.Process(target=auto_daily_debit, daemon=True)
+    p.start()
     main()
